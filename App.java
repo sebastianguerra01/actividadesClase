92dafee9d234e637b5ca44e1bcada1d5275343f5
Sebastián Guerra <105741842+sebastianguerra01@users.noreply.github.com>
2022-11-23 15:31:44 -0500

    Add files via upload
    xddd

---
+++ App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[ ] args){
+        int a = 1000 , b = 50;
+        if (a>b){
+            System.out.println("El mayor es: " +a);
+        }else{
+            if(a==b)
+                System.out.println("Son iguales");
+            else 
+            System.out.println("El mayor es: " +b);
+        }
+    
+    }
+}
---
+++ App2.java
@@ -0,0 +1,12 @@
+public class App2 {
+    public static void main(String[ ] args){
+        int a = 50 , b = 50;
+        if (a==b)
+            System.out.println("Son iguales");
+        if (a>b)
+            System.out.println("El mayor es: " +a);
+        if (a<b)
+            System.out.println("El mayor es: " +b);
+    
+    }
+}
